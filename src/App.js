991f5d4b60a6296ecf4e0af79aa6f235c6bdd9aa
Gunjan Sonawane <127420334+gunjansonaw@users.noreply.github.com>
2024-06-24 12:50:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
-function App() {
+import './assets/mail.png'
+import './assets/call.png'
+import './assets/cake.png'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form className="contact-form">
+        
+        <div className="form-group">
+          <label htmlFor="firstname">
+          <img src={'./assets/'} alt="First Name" className="label-image" />
+          </label>
+          <input type="text" name="firstname" id="firstname" placeholder="First Name" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="lastname"></label>
+          <input type="text" name="lastname" id="lastname" placeholder="Last Name" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email"></label>
+          <input type="email" name="email" id="email" placeholder="Email" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="contact"></label>
+          <input type="tel" name="contact" id="contact" placeholder="Contact" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="dob"></label>
+          <div className="dob-group">
+            <input type="number" name="day" id="day" placeholder="Day" min="1" max="31" />
+            <input type="number" name="month" id="month" placeholder="Month" min="1" max="12" />
+            <input type="number" name="year" id="year" placeholder="Year" min="1900" max="2099" />
+          </div>
+        </div>
+
+      </form>
     </div>
   );
 }
-
-export default App;
